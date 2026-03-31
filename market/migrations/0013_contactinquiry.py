2af40b8f7f1c9ffa0aa1ffdfef171679084c585b
pruthvi4344 <pruthvirathod2002@gmail.com>
2026-03-30 23:50:36 -0400

    Resolves conflict

--- market/migrations/0013_contactinquiry.py
+++ market/migrations/0013_contactinquiry.py
@@ -6,34 +6,10 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('market', '0009_artstyle_category'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('market', '0012_merge_0010_feedback_0011_profile_is_artist'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='profile',
-            name='is_artist',
-            field=models.BooleanField(default=False),
-        ),
-        migrations.CreateModel(
-            name='ArtistApplication',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('full_name', models.CharField(max_length=200)),
-                ('artist_statement', models.TextField()),
-                ('portfolio_url', models.URLField(blank=True, null=True)),
-                ('years_of_experience', models.IntegerField()),
-                ('specialization', models.CharField(max_length=100)),
-                ('certifications', models.TextField(blank=True, null=True)),
-                ('is_approved', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'ordering': ['-created_at'],
-            },
-        ),
         migrations.CreateModel(
             name='ContactInquiry',
             fields=[
@@ -46,23 +22,6 @@ class Migration(migrations.Migration):
             ],
             options={
                 'verbose_name_plural': 'Contact inquiries',
-                'ordering': ['-created_at'],
-            },
-        ),
-        migrations.CreateModel(
-            name='Feedback',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('feedback_type', models.CharField(choices=[('bug', 'Bug Report'), ('feature', 'Feature Request'), ('improvement', 'Improvement Suggestion'), ('compliment', 'Compliment'), ('complaint', 'Complaint'), ('other', 'Other')], max_length=20)),
-                ('subject', models.CharField(max_length=200)),
-                ('message', models.TextField()),
-                ('overall_rating', models.IntegerField(choices=[(5, '5 - Excellent'), (4, '4 - Good'), (3, '3 - Average'), (2, '2 - Poor'), (1, '1 - Very Poor')])),
-                ('email', models.EmailField(blank=True, max_length=254, null=True)),
-                ('allow_contact', models.BooleanField(default=False)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='feedbacks', to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
                 'ordering': ['-created_at'],
             },
         ),
